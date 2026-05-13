16ea50ee838934a661cd2240cbaebd5567ea44de
alon <alonba@liquidity-capital.com>
2026-05-13 11:14:05 +0400

    app changes broken

--- app/main.go
+++ app/main.go
@@ -42,7 +42,7 @@ func main() {
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		atomic.AddUint64(&requestCount, 1)
 		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte("I'm running after app change\n"))
+		_, _ = w.Write([]byte("I'm running after app change 2\n"))
 	})
 
 	http.HandleFunc("/readyz", func(w http.ResponseWriter, r *http.Request) {
